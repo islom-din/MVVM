35730c1e2fd4cc2021a470935b5f9c61ef9a4c1c
Islom Nuridinov <islom.din.nur@gmail.com>
2020-10-30 19:57:54 +0500

    added: Model, Repository, ViewModel

--- app/src/main/java/islom/din/architecturecomponents/MainActivity.java
+++ app/src/main/java/islom/din/architecturecomponents/MainActivity.java
@@ -1,14 +1,33 @@
 package islom.din.architecturecomponents;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.app.Application;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        noteViewModel = new ViewModelProvider(this,  ViewModelProvider.AndroidViewModelFactory.getInstance(this.getApplication())).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+                // update Recycler View
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
